d16cffc933365f7f5194bf98bc36ce532a3e9bf4
Gabrielle Oliveira <biela.a09@gmail.com>
2021-06-25 21:45:55 -0300

    Adicionando page, github.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from '../pages/Home'
-import Simpsons from '../pages/Simpsons'
+import Home from "../pages/Home";
+import GitHub from "../pages/GitHub";
+import Simpsons from "../pages/Simpsons";
 
-const Routes = () => {
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/simpsons' component={Simpsons} />
-            </Switch>
-        </BrowserRouter>
-    )
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/github" component={GitHub} />
+        <Route path="/simpsons" component={Simpsons} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default Routes
+export default Routes;
